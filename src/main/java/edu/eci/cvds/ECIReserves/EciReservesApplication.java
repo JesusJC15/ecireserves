b16291634c3fdccdfd1f89e4f07f610101312128
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-02-14 13:28:11 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/edu/eci/cvds/ECIReserves/EciReservesApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ECIReserves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EciReservesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciReservesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/ECIReserves/EciReservesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ECIReserves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciReservesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
