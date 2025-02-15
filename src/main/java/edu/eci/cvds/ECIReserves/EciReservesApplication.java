b32fc92d705c055d597c5b630d92b3200494bf58
Jesus <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-02-14 23:13:39 -0500

    Configuración inicial del backend

--- src/main/java/edu/eci/cvds/ECIReserves/EciReservesApplication.java
+++ src/main/java/edu/eci/cvds/ECIReserves/EciReservesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcireservesApplication {
+public class EciReservesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcireservesApplication.class, args);
+		SpringApplication.run(EciReservesApplication.class, args);
 	}
 
 }
