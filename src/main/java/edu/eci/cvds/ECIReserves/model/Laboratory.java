7113bcf62f23d7c221259f7648793dc4ba94f14e
unknown <santiago.hurtado-m@labinfo.is.escuelaing.edu.co>
2025-02-27 12:32:55 -0500

    Actualización metodos en servicio

--- src/main/java/edu/eci/cvds/ECIReserves/model/Laboratory.java
+++ src/main/java/edu/eci/cvds/ECIReserves/model/Laboratory.java
@@ -1,5 +1,6 @@
 package edu.eci.cvds.ECIReserves.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 
 @Data
@@ -21,8 +23,10 @@ public class Laboratory {
     private int capacity;
     private Integer computers;
     private String description;
-    private LocalDateTime openingTime;
-    private LocalDateTime closingTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+    private Date openingTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+    private Date closingTime;
     private boolean available;
 
 }
--- src/main/java/edu/eci/cvds/ECIReserves/repository/LaboratoryRepository.java
+++ src/main/java/edu/eci/cvds/ECIReserves/repository/LaboratoryRepository.java
@@ -1,24 +1,35 @@
 package edu.eci.cvds.ECIReserves.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.eci.cvds.ECIReserves.model.Laboratory;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface LaboratoryRepository extends MongoRepository<Laboratory, String> {
 
 
+    @Query("{ 'name' : ?0 }")
     List<Laboratory> findByName(String name);
-
+    @Query("{ 'computers' : { $gte: ?0 } }")
     List<Laboratory> findByComputersGreaterThanEqual(int minComputers);
 
+    @Query("{ 'openingTime' : { $gte: ?0 }, 'closingTime' : { $lte: ?1 } }")
+    List<Laboratory> findByOpeningTimeBetween(Date start, Date end);
+
+    @Query("{ 'openingTime' : ?0 }")
     List<Laboratory> findByOpeningTime(LocalDateTime openingTime);
-
+    @Query("{ 'closingTime' : ?0 }")
     List<Laboratory> findByClosingTime(LocalDateTime closingTime);
-
+    @Query("{ 'capacity' : { $gte: ?0 } }")
     List<Laboratory> findByCapacityGreaterThanEqual(int minCapacity);
 }
+
+
+
+
