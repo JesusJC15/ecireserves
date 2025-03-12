c45d17e2cb4aaa0d81dc87a6f28cc1f39b99cfc8
Jesus <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-03-11 20:21:59 -0500

    Backend of the project

--- src/main/java/edu/eci/cvds/ECIReserves/controller/ReservationController.java
+++ src/main/java/edu/eci/cvds/ECIReserves/controller/ReservationController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +24,6 @@ import edu.eci.cvds.ecireserves.service.ReservationService;
 
 @RestController
 @RequestMapping("/api/reservations")
-@CrossOrigin(origins = "*")
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
--- src/main/java/edu/eci/cvds/ECIReserves/controller/UserController.java
+++ src/main/java/edu/eci/cvds/ECIReserves/controller/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import edu.eci.cvds.ecireserves.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
