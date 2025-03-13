51f0529d0842e128ac9b3bfc508630f1cd993749
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-03-13 13:37:33 -0500

    Conexion con Mongo Atlas

--- src/main/java/edu/eci/cvds/ECIReserves/EciReservesApplication.java
+++ src/main/java/edu/eci/cvds/ECIReserves/EciReservesApplication.java
@@ -2,10 +2,12 @@ package edu.eci.cvds.ecireserves;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import lombok.Generated;
 
 @SpringBootApplication
+@EnableMongoRepositories
 @Generated
 public class EciReservesApplication {
 
