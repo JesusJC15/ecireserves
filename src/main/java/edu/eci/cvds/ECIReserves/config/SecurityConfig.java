9b1ce4f487344373735bd4f1a5c09f3ad09ae0d6
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-03-22 22:32:05 -0500

    Realización de pruebas unitarias para servicio de autenticación

--- src/main/java/edu/eci/cvds/ECIReserves/config/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/ECIReserves/config/SecurityConfig.java
@@ -13,15 +13,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-            .csrf().disable()
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/login", "api/auth/register").permitAll()
                 .anyRequest().authenticated()
-            )
+                )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-            .formLogin().defaultSuccessUrl("/api/reservations", true)
-            .and()
-            .logout().logoutUrl("api/logout").logoutSuccessUrl("/api/auth/login");
+            .formLogin(login -> login.defaultSuccessUrl("/api/reservations", true))
+            .logout(logout -> logout.logoutUrl("api/logout").logoutSuccessUrl("/api/auth/login"));
         return http.build();
     }
 
