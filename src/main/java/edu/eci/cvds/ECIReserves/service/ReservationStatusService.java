e973056396fb20209bf143d945bdd362deafd6b2
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-03-27 12:44:59 -0500

    correcciones en reservationstatusservice

--- src/main/java/edu/eci/cvds/ECIReserves/service/ReservationStatusService.java
+++ src/main/java/edu/eci/cvds/ECIReserves/service/ReservationStatusService.java
@@ -8,7 +8,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import edu.eci.cvds.ecireserves.enums.ReservationStatus;
+import edu.eci.cvds.ecireserves.exception.EciReservesException;
+import edu.eci.cvds.ecireserves.model.Laboratory;
 import edu.eci.cvds.ecireserves.model.Reservation;
+import edu.eci.cvds.ecireserves.repository.LaboratoryRepository;
 import edu.eci.cvds.ecireserves.repository.ReservationRepository;
 
 @Service
@@ -16,14 +19,25 @@ public class ReservationStatusService {
 
     @Autowired
     private ReservationRepository reservationRepository;
+    @Autowired
+    private LaboratoryRepository laboratoryRepository;
 
+    /**
+     * This method is used to change the status of the reservations that have already expired
+     * and update the status of the laboratory
+     * @throws EciReservesException
+     */
     @Scheduled(cron = "0 0 * * * ?")
-    public void cleanupReservations() {
+    public void cleanupReservations() throws EciReservesException {
         LocalTime now = LocalTime.now();
         List<Reservation> expired = reservationRepository.findByEndTimeBeforeAndStatus(now, ReservationStatus.AGENDADA);
         if(!expired.isEmpty()) {
             for(Reservation reservation : expired) {
                 reservation.setStatus(ReservationStatus.FINALIZADA);
+                Laboratory lab = laboratoryRepository.findById(reservation.getLaboratoryId())
+                        .orElseThrow(() -> new EciReservesException(EciReservesException.LABORATORY_NOT_FOUND));
+                lab.removeTimeSlot(reservation.getDate(), reservation.getStartTime(), reservation.getEndTime());
+                laboratoryRepository.save(lab);
                 reservationRepository.save(reservation);
             }
         }
