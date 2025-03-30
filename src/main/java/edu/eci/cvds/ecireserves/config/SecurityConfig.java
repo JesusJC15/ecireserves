3f5503ed286b5fd6b2380d952ac7f0106d82716b
Jesus <jesus.jauregui-c@mail.escuelaing.edu.co>
2025-03-30 17:58:47 -0500

    Origin agregado

--- src/main/java/edu/eci/cvds/ecireserves/config/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/ecireserves/config/SecurityConfig.java
@@ -84,7 +84,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://ecireserves-bfccasdkhxcwgnev.canadacentral-01.azurewebsites.net", "https://ecireserves-bfccasdkhxcwgnev.canadacentral-01.azurewebsites.net/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://ecireserves-bfccasdkhxcwgnev.canadacentral-01.azurewebsites.net"
+            ,"https://ecireserves-bfccasdkhxcwgnev.canadacentral-01.azurewebsites.net/"
+            ,"http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
